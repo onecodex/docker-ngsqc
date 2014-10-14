a442ecdc70beb63b667299b6ab82743870fa29b7
Nik Krumm <nkrumm@gmail.com>
2014-10-14 10:32:15 -0700

    Add multiple file handling to fastqc_json + restructure JSON output

--- fastqc_json.py
+++ fastqc_json.py
@@ -9,40 +9,50 @@ import os
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
-    parser.add_argument("reads", help="Input FASTQ, SAM, or BAM file")
+    parser.add_argument("reads", nargs="+", help="Input FASTQ, SAM, or BAM file(s)")
     parser.add_argument("output", help="Output path for QC file (json formatted)")
     parser.add_argument("--fastqc", default="fastqc", help="path to fastqc executable")
     args = parser.parse_args()
+    
+    outjson = {}
+    
+    for read_filename in args.reads:
+        outdir = tempfile.mkdtemp()
+        out = {} 
+        cmd = [args.fastqc, read_filename, "--extract", "-o", outdir]
+        r = subprocess.call(cmd)
+        if r != 0:
+            out["status"] = "error"
+            out["message"] = "FastQC failed to finish successfully"
+        else:
+            out["status"] = "success"
+            out["modules"] = []
+            # get the fastqc data filename
+            fastqc_data_dir = os.path.splitext(os.path.basename(read_filename))[0] + "_fastqc"
+            fastqc_data_filename = os.path.join(outdir, fastqc_data_dir, "fastqc_data.txt")
 
-    outdir = tempfile.mkdtemp()
+            with open(fastqc_data_filename) as f:
+                for line in f.readlines():
+                    line = line.strip() # trim endline
+                    if (line == ">>END_MODULE"):
+                        out["modules"].append(d)
+                    elif (line[0:2] == "##"):
+                        out["FastQC Version"] = line.split("\t")[1]
+                    elif (line[:2] == ">>" and line[:12] != ">>END_MODULE"): # for each module grab summary data
+                        module_name = line[2:-5] # grab module name
+                        d = {}
+                        d["name"] = module_name
+                        d["status"] = line[-4:] # and overall status pass/warn/fail
+                    elif (line[:2] != ">>" and line[0] == "#"): # grab details under each module
+                        d["data"] = []
+                        cols = line.lstrip("#").split("\t")
+                    else:
+                        vals = line.split("\t")
+                        d["data"].append(dict(zip(cols, vals)))
 
-    cmd = [args.fastqc, args.reads, "--extract", "-o", outdir]
-    r = subprocess.call(cmd)
-    if r != 0:
-        # raise exception instead?
-        raise Exception("FastQC failed!")
+        # save data in outjson, indexed by filename
+        outjson[os.path.basename(read_filename)] = out
+    
+    json.dump(outjson, open(args.output, 'w'), indent=4)
 
-    out = {}
-    outsubdir = os.path.splitext(os.path.basename(args.reads))[0] + "_fastqc"
-    data_filename = os.path.join(outdir, outsubdir, "fastqc_data.txt")
-
-    with open(data_filename) as f:
-        for line in f.readlines():
-            line = line.strip() # trim endline
-            if (line == ">>END_MODULE"):
-                out[module_name] = d
-            elif (line[0:2] == "##"):
-                out["FastQC Version"] = line.split("\t")[1]
-            elif (line[:2] == ">>" and line[:12] != ">>END_MODULE"): # for each module grab summary data
-                module_name = line[2:-5] # grab module name
-                d = {}
-                d["status"] = line[-4:] # and overall status pass/warn/fail
-            elif (line[:2] != ">>" and line[0] == "#"): # grab details under each module
-                d["data"] = []
-                cols = line.lstrip("#").split("\t")
-            else:
-                vals = line.split("\t")
-                d["data"].append(dict(zip(cols, vals)))
-
-    json.dump(out, open(args.output, 'w'), indent=4)
 
